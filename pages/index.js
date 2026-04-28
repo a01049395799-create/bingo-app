4c8531844b4054ab653a0f4a03fc2f33b15abdeb
a01049395799-create <a01049395799@gmail.com>
2026-04-28 14:21:42 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-
+export default function Home() {
+  return (
+    <div style={{ padding: 30, fontFamily: "Arial" }}>
+      <h1>빙고</h1>
+      <p>작은일도 최선을 다해요 💚</p>
+      <h2>빙고 앱 새 프로젝트 시작 완료</h2>
+    </div>
+  );
+}
